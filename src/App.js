2e383ecc37013bd460408b2e1816692cbecced13
SamC <villa151@umn.edu>
2022-07-21 08:02:58 -0500

    footer
    added footer with website's info

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ export default function App() {
   return (
     <div className="App">
       <h1>PROJECT WEATHER</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://effulgent-gingersnap-c33a99.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Samantha Cabrera
+        </a>{" "}
+        on{" "}
+        <a
+          href="https://github.com/villa151/project-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
