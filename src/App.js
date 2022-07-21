623a1e7dac46e370fa18e4b118b6389c9f10e4fe
SamC <villa151@umn.edu>
2022-07-21 08:26:15 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>PROJECT WEATHER</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://effulgent-gingersnap-c33a99.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Samantha Cabrera
-        </a>{" "}
-        on{" "}
-        <a
-          href="https://github.com/villa151/project-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>PROJECT Weather</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://effulgent-gingersnap-c33a99.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Samantha Cabrera
+          </a>{" "}
+          on{" "}
+          <a
+            href="https://github.com/villa151/project-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
