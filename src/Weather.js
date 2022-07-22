3b8861d57a7048652f1b4acdcb223b96862314fe
SamC <villa151@umn.edu>
2022-07-22 10:55:25 -0500

    css two
    second part of css changes

--- src/Weather.js
+++ src/Weather.js
@@ -12,26 +12,32 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-warning" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-warning w-100"
+            />
           </div>
         </div>
       </form>
       <br />
       <h3 className="date">Saturday, June 22</h3>
       <h1 className="cityResult">Isla Mujeres</h1>
-      <h2>35°C</h2>
+      <h2 className="temperature">35</h2>
+      <span className="unit">°C</span>
       <br />
       <div className="row">
-        <div className="col-5">
+        <div className="col-4">
           <ul>
             <li>Humidity: 80%</li>
             <li>Feels like: 40°</li>
           </ul>
         </div>
-        <div className="col-2">
+        <div className="col-3">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="cloudy"
