7f7f7b9432edef4fe7ff6ba0fcfbf6f8b1bb3bc5
SamC <villa151@umn.edu>
2022-07-22 13:44:37 -0500

    conditional
    added if statement

--- src/Weather.js
+++ src/Weather.js
@@ -4,60 +4,69 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
   }
-  const apiKey = "8c9e2e229b27479d87f45960af4a2ad3";
-  let city = "Guadalajara";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Weather">
-      <form>
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-warning w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <br />
+        <h3 className="date">Saturday, June 22</h3>
+        <h1 className="cityResult">Isla Mujeres</h1>
+        <h2 className="temperature">{temperature}</h2>
+        <span className="unit">°C</span>
+        <br />
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+          <div className="col-4">
+            <ul>
+              <li>Humidity: 80%</li>
+              <li>Feels like: 40°</li>
+            </ul>
           </div>
           <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-warning w-100"
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="cloudy"
             />
           </div>
-        </div>
-      </form>
-      <br />
-      <h3 className="date">Saturday, June 22</h3>
-      <h1 className="cityResult">Isla Mujeres</h1>
-      <h2 className="temperature">35</h2>
-      <span className="unit">°C</span>
-      <br />
-      <div className="row">
-        <div className="col-4">
-          <ul>
-            <li>Humidity: 80%</li>
-            <li>Feels like: 40°</li>
-          </ul>
-        </div>
-        <div className="col-3">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="cloudy"
-          />
-        </div>
-        <div className="col-5">
-          <ul>
-            <li>Wind Speed: 10m/s</li>
-            <li>Prescipitation: 15%</li>
-          </ul>
+          <div className="col-5">
+            <ul>
+              <li>Wind Speed: 10m/s</li>
+              <li>Prescipitation: 15%</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8c9e2e229b27479d87f45960af4a2ad3";
+    let city = "Guadalajara";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
