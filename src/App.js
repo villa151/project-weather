6e23d2eea8bb0cd3675be93be2ab248f2d747481
SamC <villa151@umn.edu>
2022-08-03 11:27:19 -0500

    fix code
    added attributes to links

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
           <a
             href="https://effulgent-gingersnap-c33a99.netlify.app/"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             Samantha Cabrera
           </a>{" "}
@@ -22,7 +22,7 @@ export default function App() {
           <a
             href="https://github.com/villa151/project-weather"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             {" "}
             GitHub
