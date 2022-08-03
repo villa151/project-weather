07ccff67ac8685b32c1f1ba00e32e8b82b6558fc
SamC <villa151@umn.edu>
2022-08-03 09:49:02 -0500

    use effect
    useEffect added and F deleted to focus on only one unit

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,39 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  if (unit === "celsius") {
-    return (
-      <div>
-        <h2 className="temperature">{Math.round(props.celsius)}</h2>
-        <span className="unit">
-          °C
-          <a href="/" onClick={showFarenheit}>
-            /°F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let farenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div>
-        <h2 className="temperature">{Math.round(farenheit)}</h2>
-        <span className="unit">
-          °F
-          <a href="/" onClick={showCelsius}>
-            /°C
-          </a>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2 className="temperature">{Math.round(props.celsius)}</h2>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
